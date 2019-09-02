40207e0b8da4517f8abd79b4e941ddf2a4d843f4
taro.kirameki <ec2-user@ip-172-31-43-68.us-east-2.compute.internal>
2019-09-02 01:27:33 +0000

    favorite

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,16 @@
+class FavoritesController < ApplicationController
+  def create
+    
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(micropost)
+    flash[:success] = "お気に入り登録しました"
+    redirect_back(fallback_location: root_url)
+  end
+
+  def destroy
+      micropost = Micropost.find(params[:micropost_id])
+      current_user.unfavorite(micropost)
+      flash[:success] = "お気に入り解除しました"
+      redirect_back(fallback_location: root_url)
+  end
+end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  
+  has_many :reverses_of_favorites, class_name: "Favorite",foreign_key: "micropost_id"
   validates :content, presence: true, length: { maximum: 255 }
 end
